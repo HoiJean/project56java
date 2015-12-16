0cb995d13e7e8e841470ed148f47580e2fc517c8
HoiJean <jean.mail92@gmail.com>
2015-12-17 00:37:20 +0100

    Remove .csv files

--- src/main/java/Data/Positions.java
+++ src/main/java/Data/Positions.java
@@ -10,8 +10,16 @@ import java.util.ArrayList;
  */
 public class Positions implements CsvParseable {
 
+    private String filename = "Connections.csv";
+
+    // Connection csv data positions
+    private final int DATE_POSITION = 0;
+    private final int VALUE_POSITION = 3;
+    private final int PORT_POSITION = 2;
+    private final int UNITID_POSITION = 1;
+
     public String getFile() {
-        return null;
+        return filename;
     }
 
     public ArrayList<String[]> parseline() {
